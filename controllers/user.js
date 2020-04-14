13bf33b206b5e4e73009b579551878c034231907
Hussien Elgabry <hussien.elgabry@live.com>
2020-04-14 17:54:21 -0500

    login and tokens

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,9 @@
 const User = require('../models/user');
 const bcrypt = require('bcryptjs');
+var jwt = require('../services/jwt');
 
-function saveUser(req, res) {
+function saveUser(req, res)
+{
     var user = new User();
     user.name = req.body.name;
     user.email = req.body.email.toLowerCase();
@@ -17,5 +19,35 @@ function saveUser(req, res) {
         else if (userStored)
             return res.status(200).send({user: userStored});
     });
-
 }
+
+function loginUser(req, res)
+{
+    var email = req.body.email;
+    var password = req.body.password;
+    User.findOne({email: email}, (err, user) => {
+        if (err)
+            return res.status(500).send({message: "Error logging in"});
+        if (user) 
+        {
+            bcrypt.compare(password, user.password, (err, check) => {
+                if (check) 
+                {
+                    if (req.body.gettoken)
+                    {
+                        return res.status(200).send({ token: jwt.createtoken(user)});
+                    }
+                    else
+                    {
+                        user.password = undefined; //hide password/ re-login
+                        return res.status(200).send({user});
+                    }
+                } 
+                else
+                    return res.status(500).send({message: "Wrong email or password."});
+            });
+        } else
+            return res.status(500).send({message: "Wrong email or password."});
+    });
+}
+   
