918b027ec7c5eeb389b9761090501b8bb5c28df2
Hussien Elgabry <hussien.elgabry@live.com>
2020-04-14 18:28:10 -0500

    adjust follow requirement

--- controllers/user.js
+++ controllers/user.js
@@ -78,13 +78,14 @@ function updateUser(req, res)
     });
 }
 
-function getUsers(req, res) {
+/*
+function getUsers(req, res)
+{
     var identity_user_id = req.user.sub;
     var itemsPerPage = 10;
     var page = 1;
-    if (req.params.page) {
+    if (req.params.page)
         page = req.params.page;
-    }
     User.find().sort('_id').paginate(page, itemsPerPage, (err, users, total) => {
         if (!users)
             return res.status(404).send({message: "Users Not Found."});
@@ -101,4 +102,4 @@ function getUsers(req, res) {
             });
         });
     });
-}
+}*/
