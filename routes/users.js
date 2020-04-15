a6e49cbfebcf3f6f13ed05bcf8c9b46374032583
Yusuf Hussein <yusuf.hussein8894@gmail.com>
2020-04-14 23:50:37 -0500

    users + user routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,24 @@
-var express = require('express');
-var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+module.exports = app => 
+{
+  const users = require("../controllers/user.js");
 
-module.exports = router;
+  var router = require("express").Router();
+
+  //create new user
+  router.post("/", users.create);
+
+  // Retrieve all users
+  router.get("/", users.findAll);
+
+  // Retrieve a single user with id
+  router.get("/:id", users.findOne);
+
+  // Update a user with id
+  router.put("/:id", users.update);
+
+  // delete an account with id
+  router.delete("/:id", users.delete);
+  
+  //app.use("/api/users", router);
+};
