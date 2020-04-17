cc3335a8ca2ba711d9ba7213e8bf5dd8aa91c5c9
Hussien Elgabry <hussien.elgabry@live.com>
2020-04-16 21:41:36 -0500

    is admin or self

--- middlewares/authJwt.js
+++ middlewares/authJwt.js
@@ -51,6 +51,48 @@ isAdmin = (req, res, next) => {
   });
 };
 
+isAdminOrSelf = (req, res, next) => {
+  User.findById(req.userId).exec((err, user) => {
+    if (err) 
+    {
+      res.status(500).send({ message: err });
+      return;
+    }
+
+    if(req.userID == req.params.id)
+    {//Self request
+      next();
+      return;
+    }
+    Role.find(
+      {
+        _id: { $in: user.roles },
+      },
+      (err, roles) => 
+      {
+        if (err) 
+        {
+          res.status(500).send({ message: err });
+          return;
+        }
+
+        for (let i = 0; i < roles.length; i++)
+        {
+          if (roles[i].name === "admin")
+          {//Admin request
+            next();
+            return;
+          }
+        }
+
+        res.status(403).send({ message: "Require Admin Role!" });
+        return;
+      }
+    );
+  });
+};
+
+
 const authJwt = {
   verifyToken,
   isAdmin,
